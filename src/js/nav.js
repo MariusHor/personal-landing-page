afc2edfc9f4c37c7b06d4b2f9b42c3ac0b54d4d5
marius <marius.horghidan@yahoo.com>
2022-07-05 23:56:47 +0300

    Reverted the last js change but made it so that when I resize from desktop to mobile, the previously opened nav menu now is closed

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,8 +5,10 @@ const hamburger = document.querySelector('.nav__hamburger');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-
-    if (window.innerWidth > 992) {
-        navMenu.classList.remove("active")
-    }
 });
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 992) {
+        navMenu.classList.remove("active");
+        hamburger.classList.remove('active');}
+  }); 
