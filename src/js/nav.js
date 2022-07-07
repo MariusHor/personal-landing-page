6ca2668f5e97415f25f20384dc002a562bd53919
marius <marius.horghidan@yahoo.com>
2022-07-07 21:52:42 +0300

    I made the dropdown menu full page on mobile plus I added the social icons on it

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,9 +1,11 @@
 const navMenu = document.querySelector('.nav__menu');
 const hamburger = document.querySelector('.nav__hamburger');
+const body = document.querySelector('body')
 
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+    body.classList.toggle('overflow-hidden')
 });
 
