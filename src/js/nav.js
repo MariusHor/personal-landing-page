80a4cd56a8d784b7e5055f14716c945d1bf96387
marius <marius.horghidan@yahoo.com>
2022-07-11 22:56:28 +0300

    I cleaned up the code and created some new components in order to stay better organzied and to to be able to reuse them in the future

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,10 +1,6 @@
-const navMenu = document.querySelector('.nav__menu');
-const hamburger = document.querySelector('.nav__hamburger');
+const navMenu = document.querySelector('.fullscreen-menu');
+const hamburger = document.querySelector('.hamburger');
 const body = document.querySelector('body')
-const navLogo = document.querySelector('.nav__logo')
-
-console.log(navLogo)
-
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
--- src/js/theme.js
+++ src/js/theme.js
@@ -2,7 +2,7 @@ const themeBtn = document.querySelector('.theme-switch')
 const moonIcon = document.getElementById('moon-icon')
 const sunIcon = document.getElementById('sun-icon')
 const body = document.querySelector('body')
-const navMenu = document.querySelector('.nav__menu')
+const navMenu = document.querySelector('.fullscreen-menu')
 let bars = document.querySelectorAll('.bar')
 
 themeBtn.addEventListener('click', () => {
@@ -12,8 +12,8 @@ themeBtn.addEventListener('click', () => {
         document.body.classList.remove('dark-theme')
         moonIcon.classList.remove('hide')
         sunIcon.classList.toggle('hide')
-        navMenu.classList.remove('nav__menu')
-        navMenu.classList.toggle('nav__menu--light')
+        navMenu.classList.remove('fullscreen-menu')
+        navMenu.classList.toggle('fullscreen-menu--light')
         for (const bar of bars) {
             bar.classList.remove('bar');
           }
@@ -24,8 +24,8 @@ themeBtn.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme')
         sunIcon.classList.remove('hide')
         moonIcon.classList.toggle('hide')
-        navMenu.classList.remove('nav__menu--light')
-        navMenu.classList.toggle('nav__menu')
+        navMenu.classList.remove('fullscreen-menu--light')
+        navMenu.classList.toggle('fullscreen-menu')
         for (const bar of bars) {
             bar.classList.remove('bar--light');
           }
