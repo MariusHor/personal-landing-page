346a68aa725c6ca682a4a2a73ae9ec9d26de59a3
marius <marius.horghidan@yahoo.com>
2022-07-15 20:26:00 +0300

    I started working on the about section

--- src/app.js
+++ src/app.js
@@ -3,6 +3,5 @@ import './scss/main.scss';
 //JavaScript
 import './js/index.js';
 
-import './img/1656702220900-removebg-preview.png'
-import './img/h-removebg-preview.png'
 import './img/logo.svg'
+import './img/profile.jpeg'
