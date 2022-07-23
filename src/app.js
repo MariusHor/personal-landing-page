64f16c6fa81be33ad0515ca38b603e1dff8bada4
marius <marius.horghidan@yahoo.com>
2022-07-23 20:51:24 +0200

    I created the skills section with a grid 3 column part that is responsive on mobile and desktop layouts

--- src/app.js
+++ src/app.js
@@ -5,5 +5,9 @@ import './js/index.js';
 
 import './img/logo.svg'
 import './img/profile.jpeg'
+import './img/backend.jpg'
 import './img/coder.png'
 import './img/triangle.png'
+import './img/design.jpg'
+import './img/frontend.jpg'
+
