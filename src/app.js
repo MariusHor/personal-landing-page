5994c04d8f2a7fff8b1e434bf058d278fa7f39e5
marius <marius.horghidan@yahoo.com>
2022-07-24 21:13:10 +0200

    I created two sections that look like a card and inside them I built some content for services that I provide and tools that I use

--- src/app.js
+++ src/app.js
@@ -5,9 +5,5 @@ import './js/index.js';
 
 import './img/logo.svg'
 import './img/profile.png'
-import './img/backend.jpg'
-import './img/hero.jpg'
-import './img/triangle.png'
-import './img/design.jpg'
-import './img/frontend.jpg'
+
 
