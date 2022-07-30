ba30e1730a27bb3966a6f5f783e9e216f56787ba
marius <marius.horghidan@yahoo.com>
2022-07-30 22:01:50 +0300

    I added js functions for onscroll animations, then I restyled the footer and made the nav container grid instead of flex so that the theme switch can sit horizontally centered

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,11 +1,17 @@
 const navMenu = document.querySelector('.fullscreen-menu--light');
 const hamburger = document.querySelector('.hamburger');
 const body = document.querySelector('body')
+const show = document.querySelectorAll('.show')
+
+console.log(show)
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     body.classList.toggle('overflow-hidden')
+    setTimeout(() => {     for (const item of show) {
+        item.classList.toggle('active')
+    } }, 500);
 });
 
 
